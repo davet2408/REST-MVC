8eac94d0672dedab5999ff5477bd1e0e8f443839
David Temple <david.temple@kcl.ac.uk>
2020-11-20 08:49:04 +0000

    Add CategoryController and tests

--- src/main/java/com/example/RESTmvc/controllers/v1/CategoryController.java
+++ src/main/java/com/example/RESTmvc/controllers/v1/CategoryController.java
@@ -1,7 +1,13 @@
 package com.example.RESTmvc.controllers.v1;
 
+import com.example.RESTmvc.api.v1.model.CategoryDTO;
+import com.example.RESTmvc.api.v1.model.CategoryListDTO;
 import com.example.RESTmvc.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +18,20 @@ public class CategoryController {
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getAllCategories() {
+
+        return new ResponseEntity<CategoryListDTO>(
+                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.getCategoryByName(name), HttpStatus.OK
+        );
     }
 }
--- src/test/java/com/example/RESTmvc/services/CategoryServiceTest.java
+++ src/test/java/com/example/RESTmvc/services/CategoryServiceTest.java
@@ -1,9 +1,20 @@
 package com.example.RESTmvc.services;
 
+import com.example.RESTmvc.api.v1.mapper.CategoryMapper;
+import com.example.RESTmvc.api.v1.model.CategoryDTO;
+import com.example.RESTmvc.domain.Category;
 import com.example.RESTmvc.repositories.CategoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class CategoryServiceTest {
 
@@ -16,13 +27,42 @@ class CategoryServiceTest {
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        categoryService = new CategoryServiceImpl(categoryRepository, CategoryMapper.INSTANCE);
     }
 
     @Test
     void getAllCategories() {
+
+        //given
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        //when
+        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+
+        //then
+        assertEquals(3, categoryDTOS.size());
     }
 
     @Test
     void getCategoryByName() {
+
+        //given
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        when(categoryRepository.findByName(anyString())).thenReturn(category);
+
+        //when
+        CategoryDTO categoryDTO = categoryService.getCategoryByName(NAME);
+
+        //then
+        assertEquals(ID, categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+
     }
 }
